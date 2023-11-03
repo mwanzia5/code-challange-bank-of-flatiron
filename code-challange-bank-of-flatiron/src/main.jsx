b85bafb98d3fc9393ee381e1fb08bb0d688e9945
MWANZIA <shem.masoo@student.moringaschool.com>
2023-11-03 20:59:40 +0300

    added Chakra ui

--- code-challange-bank-of-flatiron/src/main.jsx
+++ code-challange-bank-of-flatiron/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
 import './index.css'
 
@@ -8,4 +8,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <ChakraProvider/>
 )
